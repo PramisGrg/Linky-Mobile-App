fa84cdbd9d2b5e6570b71f5466ef8d221198af90
PramisGrg <gurungpramis361@gmail.com>
2025-01-23 01:13:17 +0545

    feat: react hook form implementation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,16 +1,78 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/layout/screen-wrapper";
 import BackButton from "@/components/ui/back-button";
-import { router } from "expo-router";
+import { hp } from "@/helpers/dimensions";
+import { theme } from "@/constants/theme";
+import Button from "@/components/ui/button";
+import Input from "@/components/ui/input";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { control, handleSubmit, reset } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values, "I am clicked");
+  };
   return (
     <ScreenWrapper bg="white">
-      <BackButton />
-      <Text>Login</Text>
+      <View style={styles.container}>
+        <BackButton />
+
+        <View>
+          <Text style={styles.welcomeText}>Hey</Text>
+          <Text style={styles.welcomeText}>Welcome Back 👋,</Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputContainerText}>
+            Please Login to continue
+          </Text>
+          <Input
+            icons={
+              <Fontisto name="email" size={24} color={theme.colors.textLight} />
+            }
+            name="email"
+            control={control}
+            placeholder="Enter a emai"
+          />
+          <Input
+            icons={
+              <AntDesign name="lock" size={24} color={theme.colors.textLight} />
+            }
+            secure={true}
+            name="password"
+            control={control}
+            placeholder="Enter a password"
+          />
+        </View>
+
+        <Button onPress={handleSubmit(onSubmit)} title="Login" />
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    gap: hp(6),
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    color: theme.colors.text,
+    fontWeight: 700,
+  },
+  inputContainerText: {
+    fontSize: hp(1.7),
+    fontWeight: 600,
+    color: theme.colors.textLight,
+  },
+  inputContainer: {
+    gap: 30,
+  },
+});
---
+++ components/ui/input.tsx
@@ -0,0 +1,54 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { ReactNode } from "react";
+import { theme } from "@/constants/theme";
+import { hp } from "@/helpers/dimensions";
+import { Control, Controller } from "react-hook-form";
+
+interface inputfieldProps {
+  placeholder: string;
+  icons: ReactNode;
+  secure?: boolean;
+  name: string;
+  control: Control<any>;
+}
+
+const Input = ({
+  placeholder,
+  icons,
+  secure = false,
+  name,
+  control,
+}: inputfieldProps) => {
+  return (
+    <View style={styles.inputContainer}>
+      <Text>{icons}</Text>
+      <Controller
+        control={control}
+        name={name}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            secureTextEntry={secure}
+            placeholder={placeholder}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    height: hp(7),
+    paddingHorizontal: 18,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 14,
+    borderColor: theme.colors.text,
+    borderWidth: 0.4,
+    borderRadius: theme.radius.xxl,
+  },
+});
